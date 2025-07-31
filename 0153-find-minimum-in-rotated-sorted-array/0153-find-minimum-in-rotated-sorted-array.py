426a19aba234980e38dab0097eb7786a6c487d5f
Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2025-07-31 07:02:49 +0530

    Time: 0 ms (100%), Space: 12.7 MB (27.36%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,9 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #Brute force
+        # n = len(nums)
+        # min = nums[n-1]
+        # for i in range(0,n):
+        #     if nums[i] < min:
+        #         min = nums[i]
+        # return min
+
+        #Optimal
         n = len(nums)
-        min = nums[n-1]
-        for i in range(0,n):
-            if nums[i] < min:
-                min = nums[i]
-        return min
+        low = 0 
+        high = n-1
+        minimum = nums[n-1]
+        while low <= high:
+            mid = (low+high)//2
+            
+            if nums[low]<=nums[mid]:
+                 minimum = min(minimum,nums[low])
+                 low = mid + 1
+            else:
+                minimum = min(minimum,nums[mid])
+                high = mid - 1
+        return minimum 
