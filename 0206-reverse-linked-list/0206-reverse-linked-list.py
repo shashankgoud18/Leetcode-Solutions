fe10b7f14582d5fe977eb1af6ec7cd5178ba870a
Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2025-08-03 12:04:07 +0530

    Time: 0 ms (100%), Space: 14.5 MB (25.23%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -9,14 +9,25 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        temp = head
-        stack = []
-        while temp is not None:
-            stack.append(temp.val)
-            temp = temp.next
-        temp = head
-        while temp is not None:
-            temp.val = stack.pop()
-            temp = temp.next
+        #Brute Force
+        # temp = head
+        # stack = []
+        # while temp is not None:
+        #     stack.append(temp.val)
+        #     temp = temp.next
+        # temp = head
+        # while temp is not None:
+        #     temp.val = stack.pop()
+        #     temp = temp.next
         
-        return head
+        # return head
+
+        #Optimal
+        temp = head
+        prev = None
+        while temp is not None:
+            front = temp.next
+            temp.next = prev
+            prev = temp
+            temp = front
+        return prev
