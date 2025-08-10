eab26fd3d9cf1a3a38eb9a0277971d32b6be263a
Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2025-08-10 18:23:52 +0530

    Time: 9 ms (34.85%), Space: 14.6 MB (22.63%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -5,20 +5,20 @@ class Solution(object):
         :rtype: int
         """
         #Brute Force
-        # hash_map = {}
-        # for num in nums:
-        #     if num in hash_map:
-        #         hash_map[num] += 1
-        #     else:
-        #         hash_map[num] = 1
+        hash_map = {}
+        for num in nums:
+            if num in hash_map:
+                hash_map[num] += 1
+            else:
+                hash_map[num] = 1
         
-        # for key in hash_map:
-        #     if hash_map[key]==1:
-        #         return key
+        for key in hash_map:
+            if hash_map[key]==1:
+                return key
         
 
         #optimal
-        ans = 0
-        for num in nums:
-            ans = ans^num
-        return ans
+        # ans = 0
+        # for num in nums:
+        #     ans = ans^num
+        # return ans
