5ad4daf67cf7326be523af612c0e91c9ebe504aa
Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2025-08-20 12:51:34 +0530

    Time: 0 ms (100%), Space: 13 MB (22.31%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -6,15 +6,12 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        lb = n
         low = 0
         high = n-1
         while low <= high:
-            mid = (low + high)//2
+            mid = (low+high)//2
             if nums[mid]>=target:
-                lb = mid
-                high = mid - 1
+                high = mid-1
             else:
-                low = mid + 1
-
-        return lb
+                low = mid+1
+        return low
