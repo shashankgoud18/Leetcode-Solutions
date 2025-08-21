491c0754aff707241cc69f5788abac7b0fee0576
Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2025-08-21 16:53:11 +0530

    Time: 84 ms (8.83%), Space: 20.8 MB (92.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,15 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        hash_map = {}
-        n = len(nums)
-        for i in range(0,n):
-            if nums[i] in hash_map:
-                 hash_map[nums[i]] += 1
-            else:
-                hash_map[nums[i]] = 1
-        for key in hash_map:
-            if hash_map[key] >= 2:
-                return True
+        # hash_map = {}
+        # n = len(nums)
+        # for i in range(0,n):
+        #     if nums[i] in hash_map:
+        #          hash_map[nums[i]] += 1
+        #     else:
+        #         hash_map[nums[i]] = 1
+        # for key in hash_map:
+        #     if hash_map[key] >= 2:
+        #         return True
             
+        # return False
+
+        nums = sorted(nums)
+        n = len(nums)
+        for i in range(0,n-1):
+            if nums[i] == nums[i+1]:
+                return True
         return False
