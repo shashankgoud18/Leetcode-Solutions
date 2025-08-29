4e20a8c871c642cf9f35114f3ebb22c8d12b22e9
Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2025-08-29 21:51:58 +0530

    Time: 0 ms (100%), Space: 13.1 MB (60.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        n = len(nums)
         hash_map = {}
-        for i in range(0,n): 
-           remaining = target - nums[i] 
-           if remaining in hash_map:
-              return[hash_map[remaining],i]
-           hash_map[nums[i]] = i
+        for i in range(0,len(nums)):
+            remaining = target-nums[i]
+            if remaining in hash_map:
+                return [hash_map[remaining],i]
+            else:
+                hash_map[nums[i]] = i
