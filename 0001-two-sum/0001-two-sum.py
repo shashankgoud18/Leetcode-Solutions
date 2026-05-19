a71973fd44cf4208038271195d8216e5d13c908e
Gangapuram Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2026-05-19 15:56:05 +0530

    Time: 1753 ms (14.22%), Space: 19.7 MB (83.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hash_map = {}
-        for i in range(0,len(nums)):
-            remaining = target-nums[i]
-            if remaining in hash_map:
-                return [hash_map[remaining],i]
-            else:
-                hash_map[nums[i]] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]+nums[j] == target:
+                    return i,j
+
+        
