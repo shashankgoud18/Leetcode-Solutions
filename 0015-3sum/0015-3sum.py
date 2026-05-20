8bc9896e9b9bb64d58735944348a68b6f08d4414
Gangapuram Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2026-05-20 10:36:40 +0530

    Time: 838 ms (23.82%), Space: 18.2 MB (93.95%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,32 @@
 class Solution(object):
     def threeSum(self, nums):
-       nums.sort()
-       n = len(nums)
-       ans = []
-       for i in range(n):
-        if i != 0 and nums[i] == nums[i-1]:
-            continue
-        j = i+1
-        k = n-1
-        while j < k:
-            total_sum = nums[i] + nums[j] + nums[k]
-            if total_sum < 0:
-                j += 1
-            elif total_sum > 0:
-                k -= 1
-            else:
-                temp = nums[i],nums[j],nums[k]
-                ans.append(temp)
-                j += 1
-                k -= 1
-                while j < k and nums[j] == nums[j-1]:
-                    j += 1
-                while k > j and nums[k] == nums[k+1]:
+        n = len(nums)
+        nums.sort()
+        result = []
+
+        for i in range(n-2):
+            
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+
+            j = i+1
+            k = n-1
+
+            while j<k:
+                Sum = nums[i] + nums[j] + nums[k]
+                if Sum == 0:
+                    temp = [nums[i],nums[j],nums[k]]
+                    result.append(temp)
+                    j += 1 
                     k -= 1
-       return ans
+                    while j<k and nums[j] == nums[j-1]:
+                        j += 1 
+                    while j<k and nums[k] == nums[k+1]:
+                        k -= 1
+                elif Sum < 0 :
+                    j += 1
+                elif Sum > 0:
+                    k -= 1 
+                
+               
+        return result
