c4aac21357cfc6a1bb5962bc877b706a560faa21
Gangapuram Shashank Goud <142642917+shashankgoud18@users.noreply.github.com>
2026-07-15 17:14:50 +0530

    Time: 1 ms (77.99%), Space: 26.3 MB (93.29%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,18 +3,25 @@ class Solution:
         """
         Do not eturn anything, modify nums in-place instead.
         """
-        ans = []
+        #Brute Force
+        # ans = []
+        # n = len(nums)
+        # k %= n
+        
+        # for i in range(n):
+        #     if i > n-k-1:
+        #         ans.append(nums[i])  
+        # start = n-k
+        # for i in range(0,start):
+        #     ans.append(nums[i])
+        
+        # nums[:] = ans
+
+        #Optimal
         n = len(nums)
         k %= n
+        nums[:] = nums[::-1]
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
         
-        for i in range(n):
-            if i > n-k-1:
-                ans.append(nums[i])
-                
         
-        start = n-k
-
-        for i in range(0,start):
-            ans.append(nums[i])
-        
-        nums[:] = ans
